22580e4858fed963198d707113f168f620c9e745
Naveen <naveen.w3master@gmail.com>
2022-05-03 17:07:36 +0530

    Create Task

--- app/Http/Controllers/Api/Tenant/TaskController.php
+++ app/Http/Controllers/Api/Tenant/TaskController.php
@@ -85,14 +85,91 @@ class TaskController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * 
+     * @OA\Post(
+     *     security={{"bearerAuth":{}}},
+     *     tags={"tasks"},
+     *     path="/tasks",
+     *     operationId="postTask",
+     *     summary="Create Task",
+     *     description="Create Task",
+     *     @OA\Parameter(name="X-Tenant", in="header", required=true, description="Tenant ID"),
+     *     @OA\RequestBody(
+     *          required=true, 
+     *          @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="type", type="string", example="lead", description=""),
+     *             @OA\Property(property="subject", type="string", example="Task subject", description=""),
+     *             @OA\Property(property="description", type="string", example="Task description", description=""),
+     *         )
+     *     ),
+     *     @OA\Response(
+     *          response=200, 
+     *          description="Successful Response",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="status", type="boolean", example=true),
+     *              @OA\Property(property="message", type="string", example="Success Message!"),
+     *              @OA\Property(
+     *                  property="data", 
+     *                  type="object",
+     *                  @OA\Property(property="id", type="string", example="1"),
+     *                  @OA\Property(property="name", type="string", example="Hyderabad Contacts", description=""),
+     *              ),
+     *          )
+     *     ),
+     *     @OA\Response(
+     *          response=401,
+     *          description="Unauthorized Response",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", example="Unauthorized!")
+     *          )
+     *     ),
+     *     @OA\Response(
+     *          response=422,
+     *          description="Validation Response",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="status", type="boolean", example=false),
+     *              @OA\Property(property="message", type="string", example="Validation Error Message!"),
+     *              @OA\Property(property="code", type="string", example="INVALID"),
+     *              @OA\Property(
+     *                  property="errors", 
+     *                  type="object",
+     *                      @OA\Property(
+     *                  property="subject", 
+     *                  type="array",
+     *                  @OA\Items(
+     *                         type="string",
+     *                         example="The selected subject is invalid."
+     *                  ),
+     *              ),
+     *                  ),
+     *              ),
+     *          )
+     *     ),
+     * )
      */
+
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'type' => 'required|in:lead,task',
+            'subject' => 'required|max:255',
+            'description' => 'nullable',
+        ]);
+        if ($validator->fails()) {
+            $this->response["code"] = "INVALID";
+            $this->response["message"] = $validator->errors()->first();
+            $this->response["errors"] = $validator->errors();
+            return response()->json($this->response, 422);
+        }
+
+        $task = new Task($request->all());
+        $task->status = Task::STATUS_ACTIVE;
+        $task->save();
+        
+        $this->response["status"] = true;
+        $this->response["message"] = __('strings.store_success');
+        return response()->json($this->response);
     }
 
     /**
