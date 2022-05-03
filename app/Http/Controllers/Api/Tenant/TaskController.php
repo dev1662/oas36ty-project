4a1b13f096ad66180f9bf44be5f7c3a237f37e95
Naveen <naveen.w3master@gmail.com>
2022-05-03 17:25:26 +0530

    Update Task

--- app/Http/Controllers/Api/Tenant/TaskController.php
+++ app/Http/Controllers/Api/Tenant/TaskController.php
@@ -184,15 +184,99 @@ class TaskController extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * 
+     * @OA\Put(
+     *     security={{"bearerAuth":{}}},
+     *     tags={"tasks"},
+     *     path="/tasks/{taskID}",
+     *     operationId="putTask",
+     *     summary="Update Task",
+     *     description="Update Task",
+     *     @OA\Parameter(name="X-Tenant", in="header", required=true, description="Tenant ID"),
+     *     @OA\Parameter(name="taskID", in="path", required=true, description="Task ID"),
+     *     @OA\RequestBody(
+     *          required=true, 
+     *          @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="type", type="string", example="lead", description=""),
+     *             @OA\Property(property="subject", type="string", example="Task subject", description=""),
+     *             @OA\Property(property="description", type="string", example="Task description", description=""),
+     *         )
+     *     ),
+     *     @OA\Response(
+     *          response=200, 
+     *          description="Successful Response",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="status", type="boolean", example=true),
+     *              @OA\Property(property="message", type="string", example="Success Message!"),
+     *          )
+     *     ),
+     *     @OA\Response(
+     *          response=401,
+     *          description="Unauthorized Response",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", example="Unauthorized!")
+     *          )
+     *     ),
+     *     @OA\Response(
+     *          response=403,
+     *          description="Forbidden Response",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", example="Forbidden!")
+     *          )
+     *     ),
+     *     @OA\Response(
+     *          response=422,
+     *          description="Validation Response",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="status", type="boolean", example=false),
+     *              @OA\Property(property="message", type="string", example="Validation Error Message!"),
+     *              @OA\Property(property="code", type="string", example="INVALID"),
+     *              @OA\Property(
+     *                  property="errors", 
+     *                  type="object",
+     *                      @OA\Property(
+     *                  property="task_id", 
+     *                  type="array",
+     *                  @OA\Items(
+     *                         type="string",
+     *                         example="The selected task_id is invalid."
+     *                  ),
+     *              ),
+     *                  ),
+     *              ),
+     *          )
+     *     ),
+     * )
      */
+
     public function update(Request $request, $id)
     {
-        //
+        $validator = Validator::make(['task_id' => $id] + $request->all(), [
+            'task_id' => 'required|exists:App\Models\Task,id',
+            'type' => 'required|in:lead,task',
+            'subject' => 'required|max:255',
+            'description' => 'nullable',
+        ]);
+        if ($validator->fails()) {
+            $this->response["code"] = "INVALID";
+            $this->response["message"] = $validator->errors()->first();
+            $this->response["errors"] = $validator->errors();
+            return response()->json($this->response, 422);
+        }
+
+        $task = Task::find($id);
+        if(!$task){
+            $this->response["message"] = __('strings.update_failed');
+            return response()->json($this->response, 422);
+        }
+
+        $task->fill($request->only(['type', 'subject', 'description']));
+        $task->update();
+
+        $this->response["status"] = true;
+        $this->response["message"] = __('strings.update_success');
+        return response()->json($this->response);
     }
 
     /**
