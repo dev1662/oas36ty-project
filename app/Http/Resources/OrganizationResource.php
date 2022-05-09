25129319116a99124483ff182a5a5a3147bb2d6e
Naveen <naveen.w3master@gmail.com>
2022-05-09 16:06:11 +0530

    OrganizationResource

--- app/Http/Resources/OrganizationResource.php
+++ app/Http/Resources/OrganizationResource.php
@@ -4,6 +4,12 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * Class OrganizationResource
+ * @package App\Http\Resources
+ * @OA\Schema(
+ * )
+ */
 class OrganizationResource extends JsonResource
 {
     /**
@@ -11,6 +17,10 @@ class OrganizationResource extends JsonResource
      *
      * @param  \Illuminate\Http\Request  $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    /**
+     * @OA\Property(format="string", title="name", default="Oas36ty", description="Organization name", property="name"),
+     * @OA\Property(format="string", title="subdomain", default="oas36ty", description="oas36ty", property="subdomain")
      */
     public function toArray($request)
     {
