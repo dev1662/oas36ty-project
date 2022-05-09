fe3831df1b20bffa3ba379687ddf5f371027717f
Naveen <naveen.w3master@gmail.com>
2022-05-09 12:22:47 +0530

    TaskComment model

---
+++ database/migrations/tenant/2022_05_09_064805_create_task_comments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTaskCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('task_comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('task_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->longText('comment')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->enum('status', ['active', 'inactive'])->default('inactive');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('task_comments');
+    }
+}
