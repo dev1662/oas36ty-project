22fd8ff150df3bfb7019e873e95885da14f5789f
Naveen <naveen.w3master@gmail.com>
2022-05-18 20:38:29 +0530

    Todo & Todo Mentions

---
+++ app/Models/ToDo.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ToDo extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    const STATUS_NOT_DONE = 'not-done';
+    const STATUS_DONE = 'done';
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id', 'task_id', 'todo', 'status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function task()
+    {
+        return $this->belongsTo(Task::class);
+    }
+}
---
+++ database/migrations/tenant/2022_05_16_051120_create_to_dos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateToDosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('to_dos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('task_id')->nullable()->constrained();
+            $table->string('todo');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->enum('status', ['not-done', 'done'])->default('not-done');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('to_dos');
+    }
+}
