5930a76b9d82895e5b4963742e0237ef07e8f8ff
Robin  kumar <robinoas36ty@gmail.com>
2022-09-16 00:59:26 +0530

    mailbox table schema

---
+++ database/migrations/tenant/2022_09_15_165156_create__mailbox_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMailboxTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mailbox', function (Blueprint $table) {
+            $table->id();
+            $table->string('message_id')->nullable();
+            $table->string('avatar')->nullable();
+            $table->string('from_name')->nullable();
+            $table->string('from_email')->nullable();
+            $table->string('to_email')->nullable();
+            $table->string('subject')->nullable();
+            $table->longText('message')->nullable();
+            $table->string('attachments')->nullable();
+            $table->string('label')->nullable();
+            $table->tinyText('isStarred',['true','false'])->default(false);
+            $table->tinyText('type',['primary','promotions','social'])->default("primary");
+            $table->dateTime('date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mailbox');
+    }
+}
---
+++ database/migrations/tenant/2022_09_15_170835_create_mailbox_email_settings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMailboxEmailSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mailbox_email_settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('mailbox_id');
+            $table->string('emails_settings_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mailbox_email_settings');
+    }
+}
