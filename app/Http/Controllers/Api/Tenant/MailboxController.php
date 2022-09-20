a834386a4a323caf23574d0435b870b61307c94d
devsindhwani <devoas36ty@gmail.com>
2022-09-20 17:24:05 +0530

    update api

--- app/Http/Controllers/Api/Tenant/MailboxController.php
+++ app/Http/Controllers/Api/Tenant/MailboxController.php
@@ -20,32 +20,32 @@ class MailboxController extends Controller
     public function fetchEmails(Request $req)
     {
         // $emails = json_decode($req->header('currrent'))->email;
-        $user_id = json_decode($_GET['currrent'])->id;
-        // return $user_id;
-        $check_assigned_emails = UserEmail::where('user_id', $user_id)->whereNotNull('emails_setting_id')->with('EmailsSetting')->get();
-        $inbound_array = [];
-        // foreach loop to get inbound details
-        foreach($check_assigned_emails as $index => $emails){
-            $email_setting_id = $emails->emails_setting_id;
-            $email_inbound = EmailInbound::where('id', $email_setting_id)->first();
-            $inbound_array[$index] = $email_inbound;
+        // $user_id = json_decode($_GET['currrent'])->id;
+        // // return $user_id;
+        // $check_assigned_emails = UserEmail::where('user_id', $user_id)->whereNotNull('emails_setting_id')->with('EmailsSetting')->get();
+        // $inbound_array = [];
+        // // foreach loop to get inbound details
+        // foreach($check_assigned_emails as $index => $emails){
+        //     $email_setting_id = $emails->emails_setting_id;
+        //     $email_inbound = EmailInbound::where('id', $email_setting_id)->first();
+        //     $inbound_array[$index] = $email_inbound;
            
 
-        }
-        // foreach loop to check inbound username
-        $result = [];
-        // return $inbound_array;
-        foreach($inbound_array as $index => $username)
-        {
-            // return $username->mail_username;
-            $result[$index]= Mailbox::where('to_email', $username->mail_username)->get();
-        }
+        // }
+        // // foreach loop to check inbound username
+        // $result = [];
+        // // return $inbound_array;
+        // foreach($inbound_array as $index => $username)
+        // {
+        //     // return $username->mail_username;
+        //     $result[$index]= Mailbox::where('to_email', $username->mail_username)->get();
+        // }
         // return count($result);
         // $check_user_emails_exists = 
-    
-        if($result){
-            $result = $result[0];
-        }
+            $result = Mailbox::all();
+        // if($result){
+        //     $result = $result[0];
+        // }
         $meta = [
             'emailsMeta' => count($result)
         ];
