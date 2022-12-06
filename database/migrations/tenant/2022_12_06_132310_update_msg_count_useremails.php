1b4bb22493352bc166573e9f844ec80549e6731e
Robin  kumar <robinoas36ty@gmail.com>
2022-12-06 13:30:20 +0530

    Update migration all emails Folder wise count

---
+++ database/migrations/tenant/2022_12_06_132310_update_msg_count_useremails.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateMsgCountUseremails extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if(!(Schema::hasColumn('user_emails','sent_msg_count'))){
+            Schema::table('user_emails', function(Blueprint $table){
+                $table->bigInteger('sent_msg_count')->default(0);
+            });
+        }
+        if(!(Schema::hasColumn('user_emails','trash_msg_count'))){
+            Schema::table('user_emails', function(Blueprint $table){
+                $table->bigInteger('trash_msg_count')->default(0);
+            });
+        }
+        if(!(Schema::hasColumn('user_emails','spam_msg_count'))){
+            Schema::table('user_emails', function(Blueprint $table){
+                $table->bigInteger('spam_msg_count')->default(0);
+            });
+        }
+        if(!(Schema::hasColumn('user_emails','draft_msg_count'))){
+            Schema::table('user_emails', function(Blueprint $table){
+                $table->bigInteger('draft_msg_count')->default(0);
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
