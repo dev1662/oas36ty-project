23b2e2b90ed8b09d7a9691b91f99690ab128be81
Robin  kumar <robinoas36ty@gmail.com>
2022-12-21 17:56:33 +0530

    Remove attachments from email body

--- resources/views/emails/auth/hello.blade.php
+++ resources/views/emails/auth/hello.blade.php
@@ -1,24 +1,3 @@
 {{-- {{$message}} --}}
 {{-- <h4>Hi,</h4><br/> --}}
 <div>{!! $template_data['body'] !!}</div>
-{{-- <br/>
-    
-<p>Have a great day!</p> --}}
-{{-- <p>{{ config('app.name') }}</p> --}}
-<br/>
-<div>
-    {{-- {{$template_data['files']}}
-     --}}
- 
-     
-     @if($template_data['files'])
-        @foreach ($template_data['files'] as $key => $file) 
-        
-            <br><a  href="{{$file}}" download>
-                <img src="{{$file}}" alt="W3Schools" width="104" height="142">
-
-                </a>
-                @endforeach
-      @endif
-    {{-- <a href=""></a> --}}
-</div>
